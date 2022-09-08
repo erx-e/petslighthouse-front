3355db8ac39406c0f142afbae7136216317e6859
erox <figueroa0cristian@gmail.com>
2022-09-07 21:56:54 -0500

    private

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  API_URL: 'http://petslighthouse.somee.com',
+  AWS_REGION: 'us-east-1',
+  AWS_KEY_ID: 'AKIA5QZLTSGGOBD7U5X5',
+  AWS_KEY_SECRET: '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',
+  BUCKET_URL: 'https://petslighthouse.s3.amazonaws.com',
+  BUCKET_NAME: 'petslighthouse',
+};
