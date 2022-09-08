2806749c0586789567000a0b58ad2711539434fe
erox <figueroa0cristian@gmail.com>
2022-09-08 15:40:58 -0500

    new api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  API_URL: 'http://petslighthouse.somee.com',
+  API_URL:
+    'http://petslighthouseapi-dev.eba-g5eu86dx.us-east-1.elasticbeanstalk.com',
   AWS_REGION: 'us-east-1',
   AWS_KEY_ID: 'AKIA5QZLTSGGAPAN6OAH',
   AWS_KEY_SECRET: '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',
