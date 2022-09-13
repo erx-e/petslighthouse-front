52e4853765fdf5239b788edb770e6174aad056d1
erox <figueroa0cristian@gmail.com>
2022-09-13 14:42:48 -0500

    set environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
 export const environment = {
-  production: true,
-  // API_URL: 'https://localhost:44387',
-  API_URL: 'https://petslighthouseapi.online',
-  AWS_REGION: 'us-east-1',
-  AWS_KEY_ID: 'AKIA5QZLTSGGAPAN6OAH',
-  AWS_KEY_SECRET: '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',
-  BUCKET_URL: 'https://petslighthouse.s3.amazonaws.com',
-  BUCKET_NAME: 'petslighthouse',
+  production: false,
+  API_URL: '',
+  AWS_REGION: '',
+  AWS_KEY_ID: '',
+  AWS_KEY_SECRET: '',
+  BUCKET_URL: '',
+  BUCKET_NAME: '',
 };
